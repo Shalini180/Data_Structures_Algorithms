2920b15fe0d765aaded3ed3db9cd822e4035c3b6
Shalini180 <66202473+Shalini180@users.noreply.github.com>
2024-03-18 15:34:54 +0530

    QuickSortRightPivot with extra space
    TC: O(NlogN)
    SC: O(N)
    Link: https://www.geeksforgeeks.org/problems/quick-sort/1

---
+++ Sorting/QuickSortRightPivot.java
@@ -0,0 +1,32 @@
+class Solution {
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high) {
+        if (low >= high)
+            return;
+
+        int id = partition(arr, low, high);
+        quickSort(arr, low, id - 1);
+        quickSort(arr, id + 1, high);
+    }
+
+    static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        ArrayList<Integer> small = new ArrayList<>();
+        ArrayList<Integer> large = new ArrayList<>();
+        for (int i = low; i < high; i++) {
+            if (arr[i] <= pivot)
+                small.add(arr[i]);
+            else
+                large.add(arr[i]);
+        }
+
+        int k = low;
+        for (int i = 0; i < small.size(); i++)
+            arr[k++] = small.get(i);
+        arr[k++] = pivot;
+        for (int i = 0; i < large.size(); i++)
+            arr[k++] = large.get(i);
+
+        return low + small.size();
+    }
+}
